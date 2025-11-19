6f655ffc958a10f71ed35669963f292a8b55a6c1
Abhiram Sagar <167952887+arsagarlupo@users.noreply.github.com>
2025-11-19 11:56:04 +0530

    Update upload endpoint in UploadForm.js

--- frontend/src/components/UploadForm.js
+++ frontend/src/components/UploadForm.js
@@ -23,7 +23,7 @@ function UploadForm({ setResultData }) {
 
     try {
       setLoading(true);
-      const response = await axios.post("https://deposit-reconcile-1.onrender.com/", formData, {
+      const response = await axios.post("https://deposit-reconcile-1.onrender.com/upload", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
 
